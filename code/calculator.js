d26c4370a3f082f58ba02f42fde412acb25f06a7
jsnicholson0527 <jim@nicholsonhome.com>
2024-09-20 11:27:50 -0700

    Update calculator.js to support new requirement of custom delimiter
    6.	Modify the application to Support 1 custom delimiter of a single character using the format: //{delimiter}\n{numbers}
    o	examples: //#\n2#5 will return 7; //,\n2,ff,100 will return 102
    o	all previous formats should also be supported

--- code/calculator.js
+++ code/calculator.js
@@ -4,8 +4,20 @@ function addNumbers(inputString) {
         return 0; // Return 0 for empty input
     }
 
-    // Split the string by commas and newlines
-    const numbers = inputString.split(/[,|\n]/).map(num => {
+    let delimiter = /[,|\n]/; // Default delimiters are comma and newline
+    let numbersString = inputString;
+
+    // Check for custom delimiter
+    if (inputString.startsWith("//")) {
+        const delimiterEndIndex = inputString.indexOf('\n');
+        if (delimiterEndIndex !== -1) {
+            delimiter = new RegExp(inputString[2]);
+            numbersString = inputString.substring(delimiterEndIndex + 1);
+        }
+    }
+
+    // Split the string by the custom delimiter or default delimiters
+    const numbers = numbersString.split(delimiter).map(num => {
         const parsedNum = Number(num);
         return isNaN(parsedNum) || parsedNum > 1000 ? 0 : parsedNum; // Convert invalid numbers or numbers > 1000 to 0
     });
@@ -17,7 +29,7 @@ function addNumbers(inputString) {
     }
 
     // Return the sum of the non-negative numbers <= 1000
-    return numbers.reduce((acc, num) => acc + (num >= 0 ? num : 0), 0);
+    return numbers.reduce((acc, num) => acc + num, 0);
 }
 
 // Usage examples
@@ -35,6 +47,8 @@ try {
     console.log(addNumbers("1\n2,3")); // Outputs: 6
     console.log(addNumbers("1\n2\n3\n4")); // Outputs: 10
     console.log(addNumbers("2,1001,6")); // Outputs: 8
+    console.log(addNumbers("//#\n2#5")); // Outputs: 7
+    console.log(addNumbers("//,\n2,ff,100")); // Outputs: 102
 } catch (error) {
     console.error(error.message); // Handle the error
 }
